07de8963646a2d41c6648c8de077c19ab00acb36
3jonn <3jonn@86.50.94.63>
2020-09-16 20:36:41 +0300

    mer juint test cases

--- prog4 MusicApp/src/app/MangaeFilesTests.java
+++ prog4 MusicApp/src/app/MangaeFilesTests.java
@@ -15,13 +15,22 @@ class MangaeFilesTests {
 	void creatFiletest() throws IOException {
 		test = new ManageFiles();
 		alb = new ManageAlbum();
-		File file = new File("../../te.txt");
+		File file = new File("hello");
+		alb.createRootAlbum("root");
 		SoundClip s = test.createFile(file);
 		test.addFileToSub(alb.getRootAlbum(), s);
-		
-//		file.createNewFile();
-//		test.createFile(file);
-//		assertEquals("hello",test.root.getlistOfFiles().getFirst().toString());
+		assertEquals("hello",alb.getRootAlbum().getlistOfFiles().get(0).toString());
 	}
-
+	@Test
+	void deleteFiletest() {
+		test = new ManageFiles();
+		alb = new ManageAlbum();
+		File file = new File("lalasång");
+		alb.createRootAlbum("root");
+		SoundClip s = test.createFile(file);
+		test.addFileToSub(alb.getRootAlbum(), s);
+		test.deleteFile(s);
+		assertEquals(null,alb.getRootAlbum().getlistOfFiles().get(0).toString());
+	}
+	
 }
