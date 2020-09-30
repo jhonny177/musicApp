94761c4d3bcb6183aa977becd6fd0885bb7abae4
Josef <nylund.josef@pm.me>
2020-09-30 17:55:09 +0300

    Undo fungerar nu ganska bra

--- prog4 MusicApp/src/app/AlbumCaretaker.java
+++ prog4 MusicApp/src/app/AlbumCaretaker.java
@@ -7,18 +7,25 @@ public class AlbumCaretaker {
 	private final int capacityCap = 10;
 	private Object saveObj;
 	private Stack<Object> undoStack = new Stack<Object>();
+	private Stack<Album> undoAlbums = new Stack<Album>();
 	private Stack<Object> redoStack = new Stack<Object>();
 	//sparar objectet och pushar det på en stack som aldrig blir större än size 10
 	public void saveState(Album album) {
 		saveObj = album.save();
 		undoStack.push(saveObj);
+		undoAlbums.push(album);
+		System.out.println("STacksize: " + undoAlbums.size());
 		if(undoStack.size()>capacityCap) {
 			undoStack.remove(capacityCap);
+			undoAlbums.remove(capacityCap);
 		}
 	}
 	//hämtar tillbaka den senaste ändringen som är högst upp på stacken
-	public void undo(Album album) {
-		album.restoreObj(undoStack.pop());
+	public Album undo(Album album) {
+		Album a = undoAlbums.pop();
+		a.restoreObj(undoStack.pop());
+		System.out.println("Stacksize: " + undoAlbums.size());
+		return a;
 	}
 	
 	public void redo(Album album) {
